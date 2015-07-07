0d066a16688b6ec5dca10eb226d9809bf5caabbc
RaymondLi <lalalic@gmail.com>
2015-07-07 19:10:36 +0800

    create cordova from gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp=require('gulp'),
-    shell=require('gulp-shell');
+    shell=require('gulp-shell'),
+    isWin=/^win/.test(process.platform);
 
 gulp.task('javascript',shell.task('watchify -d index.js -o www/index.js --ignore jquery'))
     .task('watchcss',function(){
@@ -17,11 +18,19 @@ gulp.task('javascript',shell.task('watchify -d index.js -o www/index.js --ignore
          * on watch tasks
          */
     })
-    .task('cordova',
-        shell.task(['cordova create cordova lalalic.superdaddy superdaddy',
-            'cd cordova',
-            'cordova platform add android ios',
-            'cordova plugin add https://github.com/litehelpers/Cordova-sqlite-storage'
-            'cordova plugin add cordova-plugin-camera',
-            'cordova plugin add https://github.com/vilic/cordova-plugin-wechat --variable APP_ID=xxxxx'
-            ]))
+    .task('cordovaCreate',shell.task(['cordova create cordova lalalic.superdaddy superdaddy --link-to=www']))
+    .task('cordovaConfig', ['cordovaCreate'], function(){
+        var fs=require('fs'),
+            path="cordova/config.xml"
+            content=fs.readFileSync(path,'utf8');
+        fs.writeFileSync(path, content.replace('src="index.html"','src="cordova.html"'));
+    })
+    .task('cordova', ['cordovaConfig'],
+                shell.task([
+                'cordova platform add android',
+                'cordova plugin add cordova-plugin-file',
+                'cordova plugin add cordova-plugin-file-transfer',
+                'cordova plugin add https://github.com/litehelpers/Cordova-sqlite-storage',
+                'cordova plugin add cordova-plugin-camera',
+                'cordova plugin add https://github.com/vilic/cordova-plugin-wechat --variable APP_ID=xxxxx'
+            ],{cwd:"cordova"}))
