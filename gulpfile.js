a438f28ddd167dd1dc18a0173b3389705b54cf82
RaymondLi <lalalic@gmail.com>
2015-07-20 18:22:40 +0800

    watch mock

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,10 @@ gulp.task('javascript',shell.task('watchify -d index.js -o www/index.js --ignore
             })
     })
     .task('css',['watchcss'],shell.task('lessc lib/css/index.less www/index.css'))
-    .task('mock',shell.task('"node_modules/.bin/restmock"'))
+    .task('watchmock', function(){
+		 gulp.watch(['mock.json'],['javascript'])
+	})
+    .task('mock',['watchmock'], shell.task('"node_modules/.bin/restmock"'))
     .task('default',['mock','css','javascript'],function(){
         /*
          * can't be here since it's blocked by mock and javascript,
