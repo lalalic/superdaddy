218ea591ba5f899bef8eaf4a040e091ee1d18c65
licheng <lalalic@139.com>
2016-12-19 23:02:27 +0800

    simple parser

--- src/knowledge/parser/index.js
+++ src/knowledge/parser/index.js
@@ -1,3 +1,33 @@
-export default function parse(){
-	
+import docx4js from "docx4js"
+import react from "react"
+import reactDOM from "react-dom/server"
+
+export default function parse(file){
+	return docx4js.load(file).then(docx=>{
+		let doc=docx.render(createComponent)
+		let html=reactDOM.renderToStaticMarkup(doc)
+		let {properties, steps, images}=doc.props
+		return {
+			html,
+			properties,
+			steps,
+			images
+		}
+	})
+}
+
+const TYPE={
+	"p":"p",
+	"r":"span",
+	"t":({children})=>children,
+	"inline.picture":"img",
+	"hyperlink":"a",
+	"tbl":"table",
+	"tr":"tr",
+	"td":"td",
+	"property":()=>null
+}
+
+function createComponent(type){
+	return TYPE[type]||"span"
 }
