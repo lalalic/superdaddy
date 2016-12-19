ade169c66692f751501460588c86a2ed8f5729eb
lalalic <lalalic@gmail.com>
2016-12-19 16:46:42 +0800

    docx parser

--- src/parser/extractor.js
+++ src/parser/extractor.js
@@ -1,49 +1,6 @@
-import docxHub from "docx-template"
-import docx4js from "docx4js"
-
 import {File} from "qili-app"
 import dbKnowledge from "../db/knowledge"
-
-import Ignore from "./html/ignore"
-import document from "./html/document"
-import documentProperty from "./html/property"
-import paragraph from "./html/p"
-import table from "./html/table"
-import row from "./html/tr"
-import cell from "./html/td"
-import text from "./html/text"
-import image from "./html/image"
-import hyperlink from "./html/hyperlink"
-import step from './html/step'
-
-var MODELS={
-	document,
-	documentProperty,
-	paragraph,
-	table,
-	row,
-	cell,
-	text,
-	image,
-	hyperlink,
-	step,
-	heading: paragraph,
-	header: Ignore,
-	footer: Ignore,
-	documentStyles: Ignore
-}
-
-class Document1 extends docx4js{
-	static Factory=class extends docx4js.Factory{
-		create(wXml, doc, parent, more){
-			let model=super.create(...arguments)
-			if(step.is(model))
-				return new step.Model(...arguments)
-
-			return model
-		}
-	}
-}
+import parse from "../knowledge/parser"
 
 function splitKey(data){
 	if(typeof(data)=='string')
@@ -54,9 +11,7 @@ function splitKey(data){
 }
 
 export default function extract(file){
-    return docxHub.assemble(file,{channel:"interactive"})
-		.then(docx=>Document1.load(docx.data))
-		.then(docx=>docx.parse(Document1.createVisitorFactory(MODELS))).then(doc=>{
+    return parse(file).then(doc=>{
         var {html:content, properties, id:elId, images, steps}=doc,
             {name,title, keywords, category, subject, abstract,description, ...others}=properties
 
