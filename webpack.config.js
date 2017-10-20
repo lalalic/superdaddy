0eb5fcafc4362db2c72e5523a4f2299ece5decd4
Li <lalalic@gmail.com>
2017-10-20 14:11:31 +0800

    relay qiliapp

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ module.exports=env=>Object.assign({
 		net: "empty",
 		module: "empty"
 	},
+	plugins:[new webpack.ContextReplacementPlugin(/graphql-language-service-interface[\/\\]dist/, /\.js$/)],
 	devServer:{
 		contentBase: path.join(__dirname, "dist"),
 		compress: true,
