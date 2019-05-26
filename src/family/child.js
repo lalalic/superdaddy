06b74cd4154fb7435213a436cae105f7867a4388
lalalic <lalalic@139.com>
2019-05-26 12:28:10 +0800

    try fix child photo

--- src/family/child.js
+++ src/family/child.js
@@ -37,7 +37,7 @@ export class Child extends Component{
 				<div className="form" style={{flex:"1 100%"}}>
 					<div className="child-photo">
 						<Photo size={150} src={photo}
-							autoUpload={{id,key:"photo.jpg"}}
+							autoUpload={{path:"photo.jpg"}}
 							onPhoto={photo=>update({photo})}/>
 					</div>
 
